410b3db6b8ad7e5d88fea1abaee40bb03473f40a
Ledezma5235 <Ledezma6163@gmail.com>
2026-07-08 10:27:14 +0000

    se instalo jest, se hizo pruebas de integracion y unitarias, se instalo cucumber.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+if (typeof module !== "undefined") {
+    module.exports = {
+        mostrarMensaje,
+        validarNombreProducto,
+        agregar
+    };
+}
 function mostrarMensaje(){
     alert("¡Docker está funcionando correctamente!");
 }
+
+function validarNombreProducto(nombre) {
+    return nombre.trim().length > 0;
+}
+
+function agregar() {
+    const nombre = document.getElementById("nombre").value;
+
+    if (validarNombreProducto(nombre)) {
+        document.getElementById("resultado").textContent =
+            "Producto válido";
+    } else {
+        document.getElementById("resultado").textContent =
+            "Producto inválido";
+    }
+}
+
---
+++ script.test.js
@@ -0,0 +1,32 @@
+const {
+    validarNombreProducto,
+    mostrarMensaje,
+    agregar
+} = require("./script");
+
+test("Un nombre válido devuelve true", () => {
+    expect(validarNombreProducto("Monitor")).toBe(true);
+});
+
+test("Un nombre vacío devuelve false", () => {
+    expect(validarNombreProducto("   ")).toBe(false);
+});
+
+test("El mensaje es correcto", () => {
+    expect(mostrarMensaje()).toBe("¡Docker está funcionando correctamente!");
+});
+test("Al agregar un producto válido se actualiza el mensaje", () => {
+
+    document.body.innerHTML = `
+        <input id="nombre">
+        <button id="btnAgregar"></button>
+        <p id="resultado"></p>
+    `;
+
+    document.getElementById("nombre").value = "Monitor";
+
+    agregar();
+
+    expect(document.getElementById("resultado").textContent)
+        .toBe("Producto válido");
+});
